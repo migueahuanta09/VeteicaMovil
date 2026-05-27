62b2ea628b17dce2e3ba54e2ef55301fe5a1325e
XxDercrashxX <zian.vazquez@gmail.com>
2026-05-27 13:44:40 -0700

    fix: corregir importacion en serviceRoutes

--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const { proteger } = require('../middleware/authMiddleware');
 const {
-  obtenerServices, obtenerService,
-  crearService, actualizarService, eliminarService,
+  obtenerServices, crearService,
+  actualizarService, eliminarService,
 } = require('../controllers/serviceController');
 
 router.get('/',    proteger, obtenerServices);
-router.get('/:id', proteger, obtenerService);
 router.post('/',   proteger, crearService);
 router.put('/:id', proteger, actualizarService);
 router.delete('/:id', proteger, eliminarService);
